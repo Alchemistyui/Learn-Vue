85142518289722e0bc020345adc1c190dccb0e42
Alchemist <1319525622@qq.com>
2017-10-26 14:07:33 +0800

    学习官网的介绍部分

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@
         {{item.label}}
       </li>
     </ul>
-
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -6,8 +6,32 @@ import App from './App'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
+var app = new Vue({
   el: '#app',
   template: '<App/>',
   components: { App }
+})
+
+var hello = new Vue ({
+	el: '#hello',
+	data: {
+		message: 'hello vue'
+	}
+})
+
+var app_2 = new Vue ({
+	el: '#app-2',
+	data: {
+		seen: false
+	}
+})
+
+var app_3 = new Vue ({
+	el: '#app_3',
+	data: {
+		todos: [
+		{item: 'no.1'},
+		{item: 'no.2'}
+		]
+	}
 })
